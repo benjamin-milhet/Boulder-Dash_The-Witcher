c296d9d200d146d985f6585b62e0f2a430edee2d
EL Sayed Raif <95072303+ElSRaif@users.noreply.github.com>
2022-03-31 15:50:17 +0200

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+document.getElementById("jouer").onclick = function (){
+    document.location.href="./html/game.html?game=start"; 
+}
+
+document.getElementById("reprendre").onclick = function (){
+    document.location.href="./html/game.html?game=reprendre"; 
+}
+
+
+document.getElementById("edition").onclick = function (){
+    document.location.href="./html/editionNiveau.html"; 
+}
